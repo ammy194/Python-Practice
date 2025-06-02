37a71343a63895faf410e4bd005b4849f572e5d4
ammy194 <amartyap19@gmail.com>
2025-06-02 23:49:42 +0530

    Updated the List.py file to include a new function for filtering items based on a specific condition.

--- List.py
+++ List.py
@@ -1,4 +1,4 @@
-a = int(input("How many numbers do you want to print? "))
+# a = int(input("How many numbers do you want to print? "))
 List = [2,4,5,6,8,12,30,43,39,54,67,89,90,100]
 for i in range(len(List)):
     if i < len(List):
@@ -8,4 +8,8 @@ for i in range(len(List)):
                
 print("The length of the list is : ", len(List))
 
-        
+for i in List:
+    if(i % 2 == 0):
+        print("The even numbers are : ", i)
+    else:
+        print("The odd numbers are : ", i)
