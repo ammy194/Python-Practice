58797cfd90a456a2118e7ff1fd14826126666ceb
ammy194 <amartyap19@gmail.com>
2025-06-04 13:21:14 +0530

    updated the Git demo.py, List.py, and added new files api_weather.py, test.py, and test_test.py

--- List.py
+++ List.py
@@ -1,6 +1,21 @@
 # This code takes an input from the user to print a specified number of elements from a predefined list.
 x = int(input("How many numbers do you want to print? "))
 List = [2,4,5,6,8,12,30,43,39,54,67,89,90,100]
+def even_Ammy(y):
+    even_numbers = []
+    for num in y:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    print("Even numbers in the list:", even_numbers)
+
+def odd_Ammy(y):
+    odd_numbers = []
+    for num in y:
+        if num % 2 != 0:
+            odd_numbers.append(num)
+    print("Odd numbers in the list:", odd_numbers)
+
+
 for i in range(x):
     if i < len(List):
         print(List[i]) 
@@ -15,18 +30,8 @@ print("The length of the list is : ", len(List))
 choice = input("Do you want to print even or odd numbers? (even/odd): ")
 
 if choice.lower() == "even":
-    even_numbers = []
-    for num in List:
-        if num % 2 == 0:
-            even_numbers.append(num)
-    print("Even numbers in the list:", even_numbers)
+    even_Ammy(List)
 
 elif choice.lower() == "odd":
-    odd_numbers = []
-    for num in List:
-        if num % 2 != 0:
-            odd_numbers.append(num)
-    print("Odd numbers in the list:", odd_numbers)
-
-
+    odd_Ammy(List)
 
---
+++ test.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# This code adds two numbers and prints the result
+# It also includes a function to perform the addition.
+
+def add(a,b):
+    return a+b
+
+print("The sum of the two numbers is:", add(a, b))
